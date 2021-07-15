9dca10acc05e6c064f6183bee451cc222285de04
Zachary <taylorzw96@gmail.com>
2021-07-14 19:53:42 -0500

    Added ability to add superhero to table

---
+++ src/components/CreateSuperhero/createSuperhero.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+
+class CreateSuperhero extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            primaryAbility: '',
+            secondaryAbility: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const newSuperhero = {
+            name: this.state.name,
+            primaryAbility: this.state.primaryAbility,
+            secondaryAbility: this.state.secondaryAbility
+        }
+        this.props.addSuperhero(newSuperhero);
+        this.setState({
+            name: '',
+            primaryAbility: '',
+            secondaryAbility: ''
+        });
+    }
+
+    render() {
+        return(
+            <React.Fragment>
+                <center>
+                    <h3>Add A Superhero</h3>
+                </center>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="row col-align">
+                        <div className="col-md-4">
+                            <label>Superhero Name</label>
+                            <input name="name" type='text' onChange={this.handleChange} value={this.state.name} />
+                        </div>
+                        <div className="row col-md-4">
+                            <label>Primary Ability</label>
+                            <input name="primaryAbility" type='text' onChange={this.handleChange} value={this.state.primaryAbility} />
+                        </div>
+                        <div className="row col-md-4">
+                            <label>Secondary Ability</label>
+                            <input name="secondaryAbility" type='text' onChange={this.handleChange} value={this.state.secondaryAbility} />
+                        </div>
+                            <button type='submit' value="Add">Submit</button>
+                    </div>
+                </form>
+            </React.Fragment>
+        );
+    }
+}
+
+export default CreateSuperhero;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,15 @@
+
 import React, {Component} from 'react';
 import AlertUser from './AlertUser/alertUser';
 import DisplayName from './DisplayName/displayName';
 import NamesList from './NamesList/namesList';
 import SuperheroTable from './Superhero/superhero';
+import CreateSuperhero from './CreateSuperhero/createSuperhero';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  
+        this.state = {
             firstName: 'Reggie',
             lastName: 'White',
 
@@ -36,18 +38,33 @@ class App extends Component {
 
         }
     }
+
+    addSuperhero = (newSuperhero) => {
+        let newSuperheroObject = {
+          name: newSuperhero.name,
+          primaryAbility: newSuperhero.primaryAbility,
+          secondaryAbility: newSuperhero.secondaryAbility
+        }
+        let tempSuperheroes = this.state.superheroes;
+        tempSuperheroes.push(newSuperheroObject);
+        this.setState({
+          superheroes: tempSuperheroes
+        });
+      }
     
-    render() { 
+
+    render() {
         return (
             <React.Fragment>
                 <DisplayName firstName={this.state.firstName} lastName={this.state.lastName} />
                 <NamesList  names={this.state.names}/>
                 <AlertUser />
                 <SuperheroTable superheroes={this.state.superheroes} />
+                <CreateSuperhero addSuperhero={this.addSuperhero} />
             </React.Fragment>
-            
+
         );
     }
 }
- 
+
 export default App;
